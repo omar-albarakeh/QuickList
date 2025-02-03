0dec30b085d0ecbbdb6e6810eefce8b00bc3d8f2
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-04 01:21:50 +0200

    floatting button updated

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -13,21 +13,54 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Quick List"),
+        title: const Text(
+          "Quick List",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.orange,
       ),
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 30,
-            right: 20,
-            child: FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(Icons.add),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 30,
+              right: 20,
+              child: FloatingActionButton(
+                onPressed: () => _showDialog(),
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Add Name"),
+          content: const TextField(
+            decoration: InputDecoration(
+              labelText: "Enter user name",
             ),
           ),
-        ],
-      ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Add"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
