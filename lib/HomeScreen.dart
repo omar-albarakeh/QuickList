ed1d43cf854d7276621ab1b47ebeb2d61fd0544e
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-03 22:59:29 +0200

    lets goo

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -11,6 +11,11 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle:true,
+              title: Text("Quick List"),
+        backgroundColor: Colors.orange,
+      ),
         body :Text("hello world"),
     );}
 }
