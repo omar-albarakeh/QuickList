19bbef52ae078e23b1a837ce15f3367bbbcc26d1
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-04 02:54:38 +0200

    updated

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -24,93 +24,95 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         title: const Text(
           "Quick List",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 35),
         ),
         backgroundColor: Colors.orange,
       ),
       body: SafeArea(
         child: _nameList.isEmpty
             ? Center(
-                child: Text("No Names Added"),
-              )
+          child: Text("No Names Added",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+        )
             : ListView.builder(
-                padding: const EdgeInsets.all(16.0),
-                itemCount: _nameList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Text("${index + 1} ."),
-                    title: Text(" ${_nameList[index]['name']}"),
-                    subtitle: Text("Count: ${_nameList[index]['count']}"),
-                    trailing: SizedBox(
-                      width: 130,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          GestureDetector(
-                            onTap: () => _updateCount(index),
-                            child: Container(
-                              width: 36,
-                              height: 36,
-                              decoration: BoxDecoration(
-                                color: Colors.green,
-                                shape: BoxShape.circle,
-                              ),
-                              alignment: Alignment.center,
-                              child: const Icon(
-                                Icons.keyboard_double_arrow_up_sharp,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 10,),
-                          GestureDetector(
-                            onTap: () => _DownGradeCount(index),
-                            child: Container(
-                              width: 36,
-                              height: 36,
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                shape: BoxShape.circle,
-                              ),
-                              alignment: Alignment.center,
-                              child: const Icon(
-                                Icons.keyboard_double_arrow_down_sharp,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 10,),
-                          GestureDetector(
-                            onTap: () => _DownGradeCount(index),
-                            child: Container(
-                              width: 36,
-                              height: 36,
-                              decoration: BoxDecoration(
-                                color: Colors.orange,
-                                shape: BoxShape.circle,
-                              ),
-                              alignment: Alignment.center,
-                              child: const Icon(
-                                Icons.refresh,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            ),
-                          ),
-                        ],
+          padding: const EdgeInsets.all(16.0),
+          itemCount: _nameList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text("${index + 1} ."),
+              title: Text(" ${_nameList[index]['name']}"),
+              subtitle: Text("Count: ${_nameList[index]['count']}"),
+              trailing: SizedBox(
+                width: 130,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: () => _updateCount(index),
+                      child: Container(
+                        width: 36,
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        ),
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.keyboard_double_arrow_up_sharp,
+                          color: Colors.white,
+                          size: 30,
+                        ),
                       ),
-
                     ),
-                  );
-                },
+                    SizedBox(width: 10,),
+                    GestureDetector(
+                      onTap: () => _DownGradeCount(index),
+                      child: Container(
+                        width: 36,
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.keyboard_double_arrow_down_sharp,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    GestureDetector(
+                      onTap: () => _DownGradeCount(index),
+                      child: Container(
+                        width: 36,
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.refresh,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
               ),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
         onPressed: () => _showDialog(),
-        child: const Icon(Icons.add ,color: Colors.white,),
+        child: const Icon(Icons.add, color: Colors.white,),
       ),
     );
   }
@@ -136,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void _DownGradeCount(index){
+  void _DownGradeCount(index) {
     setState(() {
       _nameList[index]['count'] -= 1;
     });
@@ -150,9 +152,20 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text("Add Name"),
+          backgroundColor: Colors.orange[50],
+          title: const Text(
+            "Add Name",
+            style: TextStyle(color: Colors.orange),
+          ),
           content: TextField(
-            decoration: const InputDecoration(labelText: "Enter User Name"),
+            decoration: const InputDecoration(
+              labelText: "Enter User Name",
+              labelStyle: TextStyle(color: Colors.orange),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                    color: Colors.orange),
+              ),
+            ),
             controller: _nameController,
           ),
           actions: [
@@ -161,18 +174,19 @@ class _HomeScreenState extends State<HomeScreen> {
                 _nameController.clear();
                 Navigator.pop(context);
               },
-              child: const Text("Cancel"),
+              child: const Text(
+                  "Cancel", style: TextStyle(color: Colors.orange)),
             ),
             TextButton(
               onPressed: () {
-                String name = _nameController.text;
+                String name = _nameController.text.trim();
                 if (name.isNotEmpty) {
                   _AddName(name);
                 }
                 _nameController.clear();
                 Navigator.pop(context);
               },
-              child: const Text("Add"),
+              child: const Text("Add", style: TextStyle(color: Colors.orange)),
             ),
           ],
         );
