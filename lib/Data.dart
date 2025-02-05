117973959a8cc55f20a90cd7ee22335b301a34fd
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-06 00:29:43 +0200

    updated

--- lib/Data.dart
+++ lib/Data.dart
@@ -1,28 +1,30 @@
 import 'package:quicklist/storage.dart';
 
-class Data{
-
-  List<Map<String,dynamic>> nameList=[];
+class Data {
+  List<Map<String, dynamic>> nameList = [];
 
   Future<void> initialize() async {
-    nameList =await Storage.LoadData();
+    nameList = await Storage.LoadData();
   }
-  //add name
-void AddName(String name){
-    if(name.isNotEmpty && !nameList.any((item)=> item['name'] == name)){
-      nameList.add({"name" : name , "Count" : 0});
+  
+  void AddName(String name) {
+    if (name.isNotEmpty && !nameList.any((item) => item['name'] == name)) {
+      nameList.add({"name": name, "Count": 0});
       Storage.saveData(nameList);
     }
-}
-  //addCounter
-void AddCounter(index){
-    nameList[index]['count'] +=1;
+  }
+
+  void AddCounter(index) {
+    nameList[index]['count'] += 1;
     Storage.saveData(nameList);
-}
-  //SubtractCounter
-void SubtractCounter(index){
-    nameList[index]['count'] -=1;
+  }
+
+  void SubtractCounter(index) {
+    nameList[index]['count'] -= 1;
     Storage.saveData(nameList);
-}
-  //RestsetCounter
+  }
+
+  void RestCounter(index) {
+    nameList[index]['count'] = 0;
+  }
 }
