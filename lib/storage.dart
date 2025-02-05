530159e7c4f4899e1edd066b8e0da71ae1476fdd
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-05 23:36:29 +0200

    updated

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Storage{
-  
-  static Future<void> saveData1(List<Map<String,dynamic>> nameList) async{
-    final prefs =await SharedPreferences.getInstance();
+class Storage {
+  static Future<void> saveData(List<Map<String, dynamic>> nameList) async {
+    final prefs = await SharedPreferences.getInstance();
     await prefs.setString("nameList", jsonEncode(nameList));
+  }
+
+  static Future<List<Map<String, dynamic>>> LoadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? storedData = prefs.getString("nameList");
+    if (storedData != null) {
+      return List<Map<String, dynamic>>.from(json.decode(storedData));
+    }
+    return [];
   }
 }
