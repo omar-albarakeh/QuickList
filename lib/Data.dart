4f4db17508ac2c4d6360a95dcf95aab473e3fc4c
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-06 00:25:58 +0200

    updated

--- lib/Data.dart
+++ lib/Data.dart
@@ -8,6 +8,12 @@ class Data{
     nameList =await Storage.LoadData();
   }
   //add name
+void AddName(String name){
+    if(name.isNotEmpty && !nameList.any((item)=> item['name'] == name)){
+      nameList.add({"name" : name , "Count" : 0});
+      Storage.saveData(nameList);
+    }
+}
   //addCounter
   //SubtractCounter
   //RestsetCounter
